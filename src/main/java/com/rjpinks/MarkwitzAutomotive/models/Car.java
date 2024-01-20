9ef06bbf1c886f2e8f8c960a6a93ad5d204249cf
Ryan <pinkstonr54@gmail.com>
2024-01-20 15:12:08 -0600

    first push. have car model established and seeded

---
+++ src/main/java/com/rjpinks/MarkwitzAutomotive/models/Car.java
@@ -0,0 +1,25 @@
+package com.rjpinks.MarkwitzAutomotive.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String vin;
+    private String make;
+    private String model;
+    private int modelYear;
+    private String carUrl;
+}
---
+++ src/test/java/com/rjpinks/MarkwitzAutomotive/MarkwitzAutomotiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjpinks.MarkwitzAutomotive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkwitzAutomotiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
